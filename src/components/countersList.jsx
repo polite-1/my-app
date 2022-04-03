be0df4d9c2ca4211c232b2c29777c1fc7cb65772
polite-1 <polite-1@yandex.ru>
2022-04-03 22:34:54 +0300

    increment and decrement

---
+++ src/components/countersList.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import Counter from './counter'
+
+const CountersList = () => {
+  const initialState = [
+    { id: 0, value: 0, name: 'Ненужная вещь' },
+    { id: 1, value: 4, name: 'Ложка' },
+    { id: 2, value: 0, name: 'Вилка' },
+    { id: 3, value: 0, name: 'Тарелка' },
+    { id: 4, value: 0, name: 'Набор минималиста' },
+  ]
+  const [counters, setCounters] = useState(initialState)
+  const handleDelete = (id) => {
+    const newCounters = counters.filter((c) => c.id !== id)
+    setCounters(newCounters)
+  }
+  const handleReset = () => {
+    setCounters(initialState)
+    console.log('handleReset')
+  }
+  const handleIncrement = (id) => {
+    setCounters(
+      counters.map((counter) =>
+        counter.id === id
+          ? { ...counter, value: counter.value + 1 }
+          : { ...counter }
+      )
+    )
+  }
+  const handleDecrement = (id) => {
+    setCounters(
+      counters.map((counter) =>
+        counter.id === id
+          ? { ...counter, value: counter.value - 1 }
+          : { ...counter }
+      )
+    )
+  }
+
+  return (
+    <>
+      {counters.map((count) => (
+        <Counter
+          key={count.id}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          {...count}
+        />
+      ))}
+      <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>
+        Сброс
+      </button>
+    </>
+  )
+}
+
+export default CountersList
